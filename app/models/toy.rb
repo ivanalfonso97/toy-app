3ede62f0720600936965e65a2ddfb4794f75b80d
ivanalfonso97 <ivanalfonso.chan@gmail.com>
2022-12-27 01:02:09 +0800

    Fix image upload bugs

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -4,6 +4,6 @@ class Toy < ApplicationRecord
 
   validates :toy_name, presence: true
   validates :description, length: { maximum: 400 }
-  validates :images, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a jpeg, gif, or png file" },
+  validates :images, content_type: { in: %w[images/jpeg images/gif images/png], message: "must be a jpeg, gif, or png file" },
                     size: { less_than: 5.megabytes, message: "Should be less than 5MB" }
 end
